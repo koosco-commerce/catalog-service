cb04d91d3005063cf485cf2ee178ca86613b4ae7
Koo <koosco136@gmail.com>
2025-12-25 01:51:23 +0900

    chore: remove h2 dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-mysql")
-    runtimeOnly("com.h2database:h2")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 
     implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
